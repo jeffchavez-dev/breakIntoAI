73078393e0414b3455c06d98ea215cd7c09b3b1d
Jeff Chavez <jeffchavez@Jeffs-MacBook-Neo.local>
2026-07-18 16:44:04 +0800

    A quick introduction to Python syntax, variable assignment, and numbers - lesson 1

--- index.py
+++ index.py
@@ -1,10 +1,13 @@
-print("breakIntoAi")
 
 spam_amount = 0
-print(spam_amount)
+
+# Ordering Spam, egg, Spam, Spam, bacon and Spam (4 more servings of Spam)
 
 spam_amount = spam_amount + 4
 print(spam_amount)
 
-if spam_amount < 5:
+if spam_amount > 0:
     print("My spam amount is too few")
+
+viking_song = "Spam " * spam_amount
+print(viking_song)
