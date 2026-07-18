57f625c58dc31d7c170bab9f5bb7d4e85ae4f5ae
Jeff Chavez <jeffchavez@Jeffs-MacBook-Neo.local>
2026-07-18 16:53:37 +0800

    lesson 1

--- index.py
+++ index.py
@@ -3,11 +3,17 @@ spam_amount = 0
 
 # Ordering Spam, egg, Spam, Spam, bacon and Spam (4 more servings of Spam)
 
-spam_amount = spam_amount + 4
-print(spam_amount)
+# spam_amount = spam_amount + 4
+# print(spam_amount)
 
-if spam_amount > 0:
-    print("My spam amount is too few")
+# if spam_amount > 0:
+#     print("My spam amount is too few")
 
-viking_song = "Spam " * spam_amount
-print(viking_song)
+# viking_song = "Spam " * spam_amount
+# print(viking_song)
+
+# print(type(spam_amount))
+# print(type(19.95))
+a = 65
+b = 60
+print(a // b)
