3cdf0e02cc820f0c74a78b175ab0d3d6d735967b
Jeff Chavez <jeff.chavez0828@gmail.com>
2026-07-22 11:44:14 +0800

    updated lesson

--- index.py
+++ index.py
@@ -68,3 +68,14 @@ print(
 
 print(str(least_difference(1, 10, 100)) + " this is nice")
 
+def mod_5(x):
+    """Return the remainder of x after dividing by 5"""
+    return x % 5
+
+print(
+    'Which number is biggest?',
+    max(100, 51, 14),
+    'Which number is the biggest modulo 5?',
+    max(100, 51, 14, key=mod_5),
+    sep='\n',
+)
