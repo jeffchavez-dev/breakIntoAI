8f29c8923864accf841c761d8c101e9e87b07e5b
Jeff Chavez <jeffchavez@Jeffs-MacBook-Neo.local>
2026-07-22 11:30:30 +0800

    lesson 2

--- index.py
+++ index.py
@@ -79,9 +79,10 @@ def call(fn, arg):
 def squared_call(fn, arg):
     """Call fn on the result of calling fn on arg"""
     return fn(fn(arg))
-
+"""
 print(
     call(mult_by_five, 1),
     squared_call(mult_by_five, 1), 
     sep='\n', # '\n' is the newline character - it starts a new line
 )
+"""
